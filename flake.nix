95e95860196b94209f28eadcdff66591adfd478b
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-01-02 13:53:11 -0500

    add terraform flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# https://github.com/stackbuilders/nixpkgs-terraform?tab=readme-ov-file#install
+{
+  inputs = {
+    nixpkgs-terraform.url = "github:stackbuilders/nixpkgs-terraform";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  nixConfig = {
+    extra-substituters = "https://nixpkgs-terraform.cachix.org";
+    extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+  };
+
+  outputs = { self, nixpkgs-terraform, nixpkgs, systems }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            terraform = nixpkgs-terraform.packages.${system}."1.10.0";
+          in
+          {
+            default = pkgs.mkShell {
+              buildInputs = [ terraform ];
+            };
+          });
+    };
+}
